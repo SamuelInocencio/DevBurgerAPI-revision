85bb90914cb5898945e1a39086bd968c2e1fd66c
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-09-13 14:35:21 -0300

    implemented database postgres

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = new Router();
 
 routes.get("/", (request, response) => {
-    return response.status(200).json({message: 'Tá funcionando!!!'})
-})
+	return response.status(200).json({ message: "Tá funcionando mesmo!!!" });
+});
 
 export default routes;
