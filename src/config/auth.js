df704549ec8b004b0da9f32c34cb035e8d56aee8
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-09-27 17:02:12 -0300

    connecting with Mongo DB

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '3cd539b7612cae90c62621982cd657e6',
+  secret: '3cd539b7612cae90c62621982cd657e8',
   expiresIn: '30d',
 };
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
 import CategoryController from './app/controllers/CategoryController';
+import OrderController from './app/controllers/OrderController';
 
 const routes = new Router();
 
@@ -23,5 +24,9 @@ routes.get('/products', ProductController.index);
 
 routes.post('/categories', upload.single('file'), CategoryController.store);
 routes.get('/categories', CategoryController.index);
+
+routes.post('/orders', OrderController.store);
+routes.get('/orders', OrderController.index);
+routes.put('/orders/:id', OrderController.update);
 
 export default routes;
