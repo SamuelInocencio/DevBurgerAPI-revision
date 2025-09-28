5e4cae682c7b779064dbb215789d41b5305acd77
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-09-28 18:19:41 -0300

    finshed the project

--- src/app.js
+++ src/app.js
@@ -18,7 +18,12 @@ class App {
 		this.app.use(express.json());
 		this.app.use('/product-file',
 			express.static(resolve(__dirname, '..', 'uploads')),
-		 );
+		);
+
+		this.app.use(
+			'/category-file',
+			express.static(resolve(__dirname, '..', 'uploads')),
+		);
 	}
 
 	routes() {
--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -92,6 +92,13 @@ class OrderController {
 		} catch (err) {
 			return response.status(400).json({ error: err.errors });
 		}
+
+          const { admin: isAdmin } = await User.findByPk(request.userId);
+        
+                if (!isAdmin) {
+                    return response.status(401).json();
+                }
+                
             const { id } = request.params;
             const { status } = request.body;
            
